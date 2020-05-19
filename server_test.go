abb8e1e83b0302faae709fc8d26c8cd38bf30c75
Theo Sun <theo.sun@sap.com>
2020-05-19 14:30:06 +0800

    refactor: unit test

--- server_test.go
+++ server_test.go
@@ -1,6 +1,8 @@
 package hsocks5
 
 import (
+	"net"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/armon/go-socks5"
@@ -13,9 +15,12 @@ func TestNewProxyServer(t *testing.T) {
 	socksServer, err := socks5.New(&socks5.Config{})
 	assert.NoError(t, err)
 
+	l, err := net.Listen("tcp", socksAddr)
+	assert.NoError(t, err)
+
 	go func() {
-		err = socksServer.ListenAndServe("tcp", socksAddr)
-		assert.NoError(t, err)
+		e := socksServer.Serve(l)
+		assert.NoError(t, e)
 	}()
 
 	p, err := NewProxyServer(&ProxyServerOption{
@@ -26,6 +31,25 @@ func TestNewProxyServer(t *testing.T) {
 
 	assert.NoError(t, err)
 	assert.False(t, p.isWithoutProxy("https://www.google.com"), "'google' should be with proxy")
+	assert.False(t, p.isWithoutProxy("https://www.google.com"), "'google' should be cached")
+	assert.False(t, p.isWithoutProxy("172.217.160.78"), "'google' ip should be with proxy")
 	assert.True(t, p.isWithoutProxy("192.168.3.1"), "'192.168.3.1' should be without proxy")
 
+	req := httptest.NewRequest("GET", "https://github.com", nil)
+	w := httptest.NewRecorder()
+	p.handleProxyRequest(w, req)
+	resp := w.Result()
+	assert.Equal(t, 200, resp.StatusCode)
+
+	req = httptest.NewRequest("Get", "/hsocks5/__/metric", nil)
+	w = httptest.NewRecorder()
+	p.handleRequest(w, req)
+	resp = w.Result()
+	assert.Equal(t, 200, resp.StatusCode)
+
+	req = httptest.NewRequest("Get", "/whatever", nil)
+	w = httptest.NewRecorder()
+	p.handleRequest(w, req)
+	resp = w.Result()
+	assert.Equal(t, 404, resp.StatusCode)
 }
